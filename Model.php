512b260bba08960d2c3d634c5221de81e48ad8ed
Thomas Steur <thomas.steur@gmail.com>
2014-01-06 04:20:04 +0000

    fix id is not defined

--- Model.php
+++ Model.php
@@ -433,7 +433,7 @@ class Model
         foreach ($alerts as &$alert) {
             $alert['additional_emails'] = json_decode($alert['additional_emails']);
             $alert['phone_numbers']     = json_decode($alert['phone_numbers']);
-            $alert['idSites']           = $this->fetchSiteIdsTheAlertWasDefinedOn($alert['id']);
+            $alert['idSites']           = $this->fetchSiteIdsTheAlertWasDefinedOn($alert['idalert']);
         }
 
         return $alerts;
