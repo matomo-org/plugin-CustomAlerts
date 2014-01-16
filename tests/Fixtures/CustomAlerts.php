958be4a20f04c43dc44bb3c8fde5a7f9a922163d
Thomas Steur <thomas.steur@gmail.com>
2014-01-16 03:31:06 +0000

    added some triggered alerts to the test fixtures

--- tests/Fixtures/CustomAlerts.php
+++ tests/Fixtures/CustomAlerts.php
@@ -39,9 +39,23 @@ class Test_Piwik_Fixture_CustomAlerts extends Test_Piwik_BaseFixture
 
         // should not show up as different login
         $this->createAlert('Test Alert 4', 'month', array($this->idSite), 'nb_hits', 'Actions_getPageUrls', 'loginnotmatching');
+
+        $this->triggerAlert(1, 10, 5, '2014-01-16 03:21:17');
+        $this->triggerAlert(1, 2999, 10, '2014-01-15 03:21:17');
+        $this->triggerAlert(2, 1004, 1, '2013-01-16 03:21:17');
+        $this->triggerAlert(2, 10, 5, '2013-10-16 03:21:17');
+
+        // should not show up as belongs to different login
+        $this->triggerAlert(4, 10, 5, '2013-10-16 03:21:17');
     }
 
-    private function createAlert($name, $period = 'week', $idSites = array(1), $metric = 'nb_visits', $report = 'MultiSites_getOne', $login = false)
+    private function triggerAlert($idAlert, $valueNew, $valueOld, $datetime)
+    {
+        $model = new Model();
+        $model->triggerAlert($idAlert, $this->idSite, $valueNew, $valueOld, $datetime);
+    }
+
+    private function createAlert($name, $period, $idSites, $metric, $report, $login = false)
     {
         if (false === $login) {
             $login = Piwik::getCurrentUserLogin();
@@ -50,8 +64,13 @@ class Test_Piwik_Fixture_CustomAlerts extends Test_Piwik_BaseFixture
         $emails = array('test1@example.com', 'test2@example.com');
         $phoneNumbers = array('0123456789');
 
+        $reportMatched = '';
+        if ('VisitsSummary_get' != $report) {
+            $reportMatched = 'Piwik';
+        }
+
         $model = new Model();
-        $model->createAlert($name, $idSites, $login, $period, 0, $emails, $phoneNumbers, $metric, 'less_than', 5, $comparedTo = 1, $report, 'matches_exactly', 'Piwik');
+        $model->createAlert($name, $idSites, $login, $period, 0, $emails, $phoneNumbers, $metric, 'less_than', 5, $comparedTo = 1, $report, 'matches_exactly', $reportMatched);
     }
 
     public function tearDown()
