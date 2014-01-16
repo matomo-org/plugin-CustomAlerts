622253a2df37d0d5b4509958297d000188e4ff4b
Thomas Steur <thomas.steur@gmail.com>
2014-01-16 03:07:11 +0000

    add a login which does not match the current login to make sure this will not be displayed

--- tests/Fixtures/CustomAlerts.php
+++ tests/Fixtures/CustomAlerts.php
@@ -36,11 +36,17 @@ class Test_Piwik_Fixture_CustomAlerts extends Test_Piwik_BaseFixture
         $this->createAlert('Test Alert 1', 'day', array($this->idSite), 'nb_visits', 'VisitsSummary_get');
         $this->createAlert('Test Alert 2', 'week', array($this->idSite), 'nb_uniq_visitors', 'Referrers_getSearchEngines');
         $this->createAlert('Test Alert 3', 'month', array($this->idSite), 'nb_hits', 'Actions_getPageUrls');
+
+        // should not show up as different login
+        $this->createAlert('Test Alert 4', 'month', array($this->idSite), 'nb_hits', 'Actions_getPageUrls', 'loginnotmatching');
     }
 
-    private function createAlert($name, $period = 'week', $idSites = array(1), $metric = 'nb_visits', $report = 'MultiSites_getOne')
+    private function createAlert($name, $period = 'week', $idSites = array(1), $metric = 'nb_visits', $report = 'MultiSites_getOne', $login = false)
     {
-        $login  = Piwik::getCurrentUserLogin();
+        if (false === $login) {
+            $login = Piwik::getCurrentUserLogin();
+        }
+
         $emails = array('test1@example.com', 'test2@example.com');
         $phoneNumbers = array('0123456789');
 
