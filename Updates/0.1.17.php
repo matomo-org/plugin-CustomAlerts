dabb2e5c91f3f8272234aa0a25d8a2aae29f96a4
Matthieu Aubry <matthieu.aubry@gmail.com>
2014-05-24 21:11:53 +1200

    Fix Error trying to execute the query 'ALTER TABLE `piwik_alertlog` CHANGE `email_me` `email_me` BOOLEAN NOT NULL DEFAULT '0''.     The error was: SQLSTATE[42S02]: Base table or view not found: 1146 Table '58323_demo.piwik_alertlog' doesn't exist

--- Updates/0.1.17.php
+++ Updates/0.1.17.php
@@ -15,13 +15,13 @@ use Piwik\Updates;
 
 /**
  */
-class Updates_0_1_16 extends Updates
+class Updates_0_1_17 extends Updates
 {
     static function getSql()
     {
         return array(
             "ALTER TABLE `" . Common::prefixTable('alert') . "` CHANGE `email_me` `email_me` BOOLEAN NOT NULL DEFAULT '0'" => 1060,
-            "ALTER TABLE `" . Common::prefixTable('alertlog') . "` CHANGE `email_me` `email_me` BOOLEAN NOT NULL DEFAULT '0'" => 1060,
+            "ALTER TABLE `" . Common::prefixTable('alert_triggered') . "` CHANGE `email_me` `email_me` BOOLEAN NOT NULL DEFAULT '0'" => 1060,
         );
     }
 
