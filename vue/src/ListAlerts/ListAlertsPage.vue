df0f2d5bf1bebdfac4b50753e85905a3bf07bd18
Lachlan Reynolds <lachlan.reynolds@innocraft.com>
2026-01-07 13:58:32 +1300

    fixed whitespace

--- vue/src/ListAlerts/ListAlertsPage.vue
+++ vue/src/ListAlerts/ListAlertsPage.vue
@@ -9,7 +9,7 @@
     class="alerts"
     :content-title="title"
   >
-    <p>{{ translate('CustomAlerts_ManageTooltip')}}</p>
+    <p>{{ translate('CustomAlerts_ManageTooltip') }}</p>
     <ListAlerts :alerts="alerts"/>
 
     <div class="ui-confirm" id="confirm">
